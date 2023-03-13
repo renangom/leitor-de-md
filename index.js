07c75da6f8331bcba1b0852ca6c147a7af25bcbb
Renan <renan23eduardo@gmail.com>
2023-03-13 19:43:13 -0300

    Tratando erros

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
 import chalk from "chalk";
 
-console.log(chalk.blue('Hello World'))
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    fs.readFile(caminhoDoArquivo, {encoding:'utf-8'}, (err, data) => {
+        if(err){
+            trataErro(err);
+        }
+        console.log(chalk.green(data));
+    })
+}
+
+pegaArquivo('./arquivos/texto.md');
